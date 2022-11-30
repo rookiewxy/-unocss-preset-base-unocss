656210639479fc6b9e88b0667b644fd3b555c41b
weixinyi <2586958565@qq.com>
2022-11-30 17:44:22 +0800

    升级

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-/*
- * @Author: wxy
- * @Description: 
- * @Date: 2022-11-28 18:31:27
- * @LastEditTime: 2022-11-28 18:31:36
- */
--- src/rules/flex.ts
+++ src/rules/flex.ts
@@ -2,21 +2,21 @@
  * @Author: wxy
  * @Description:
  * @Date: 2022-11-28 17:40:11
- * @LastEditTime: 2022-11-28 17:50:21
+ * @LastEditTime: 2022-11-30 17:40:46
  */
 import type { Rule } from "@unocss/core";
 
 export const flex: Rule[] = [
-  ["f-x", { display: "f-x" }],
-  ["j-c", { display: "justify-content: center" }],
-  ["j-b", { display: "justify-content: space-between" }],
-  ["j-a", { display: "justify-content: space-around" }],
-  ["j-s", { display: "justify-content: flex-start" }],
-  ["j-e", { display: "justify-content: flex-end" }],
-  ["a-c", { display: "align-items: center" }],
-  ["a-s", { display: "align-items: flex-start" }],
-  ["a-e", { display: "align-items: flex-end" }],
-  ["f-w", { display: "flex-wrap: wrap" }],
-  ["f-n-w", { display: "flex-wrap: nowrap" }],
-  ["f-x-c", { display: "flex", "flex-direction": "column" }],
+  ["f-x", { "display": "flex" }],
+  ["j-c", { "justify-content": "center" }],
+  ["j-b", { "justify-content": "space-between" }],
+  ["j-a", { "justify-content": "space-around" }],
+  ["j-s", { "justify-content": "flex-start" }],
+  ["j-e", { "justify-content": "flex-end" }],
+  ["a-c", { "align-items": "center" }],
+  ["a-s", { "align-items": "flex-start" }],
+  ["a-e", { "align-items": "flex-end" }],
+  ["f-w", { "flex-wrap": "wrap" }],
+  ["f-n-w", { "flex-wrap": "nowrap" }],
+  ["f-x-c", { "display": "flex", "flex-direction": "column" }],
 ];
